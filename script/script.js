e552041e5a19deb546b86459f33ffbe7c8ee4c52
Iassine Iahaia <zinhocabila58@gmail.com>
2024-12-05 14:09:16 +0200

    adicionado image slider

---
+++ script/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var slider = tns({
+    container: '.my-slider',
+      items: 1,
+      gutter: 30,
+      mouseDrag: true,
+      controls: true,
+      nav: true,
+      autoHeight: true,
+      autoplay: true,
+      autoplayButtonOutput: false,
+      autoplayTimeout: 3000,
+      speed: 500,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 2,
+        },
+        1024: {
+          items: 3,
+        },
+      },
+  });
+});
