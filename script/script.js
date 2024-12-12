6e96b0556e2d470c66b3693f60a893f588be9e5d
Iassine Iahaia <zinhocabila58@gmail.com>
2024-12-12 11:09:23 +0200

    display items slider

--- script/script.js
+++ script/script.js
@@ -29,3 +29,37 @@ document.addEventListener('DOMContentLoaded', () => {
       },
   });
 });
+
+// tiny-slider-main
+document.addEventListener('DOMContentLoaded', () => {
+  var slider = tns({
+    container: '.tiny-slider-main',
+    items: 1,
+    gutter: 0,
+    mouseDrag: true,
+    controls: true,
+    nav: false,
+    autoHeight: true,
+    autoplay: false,
+    controlsContainer: '.tns-controls', 
+    autoplayButtonOutput: false,
+    autoplayTimeout: 3000,
+    speed: 500,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 2,
+        gutter: 24,
+      },
+      1440: {
+        items: 2,        
+        gutter: 24,      
+      },
+    },
+  });
+});
