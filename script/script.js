2be5c1e21934335c2e778b8fd35bd7fb62b9ca5a
Iassine Iahaia <zinhocabila58@gmail.com>
2024-12-18 11:19:14 +0200

    fixed pixel perfect

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   var slider = tns({
     container: '.tiny-slider',
       items: 1,
-      gutter: 0,
+      gutter: 24,
       mouseDrag: true,
       controls: true,
       nav: false,
